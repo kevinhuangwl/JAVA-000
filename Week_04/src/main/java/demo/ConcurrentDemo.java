3bfd40d86b77e9758f1b15de43794418a4526335
huangwl <huangwl@kamfu.com.cn>
2020-11-09 22:20:32 +0800

    提交作业

---
+++ Week_04/src/main/java/demo/ConcurrentDemo.java
@@ -0,0 +1,130 @@
+package demo;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 思考有多少种方式，在 main 函数启动一个新线程，运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * @author kevinhuangwl
+ *
+ */
+public class ConcurrentDemo {
+	
+	static ExecutorService exec = Executors.newSingleThreadExecutor();
+
+	public static void main(String[] args) throws Exception {
+		spinWaiting();
+		useWaitAndNotify();
+		useCompletableFuture();
+		useCountDownLatch();
+		useCyclicBarrier();
+		useFuture();
+		useJoin();
+		useSemaphore();
+		exec.shutdown();
+	}
+	
+	private static int fibo(int n) {
+		if(n == 1) return 1;
+		if(n == 2) return 2;
+		return fibo(n-1) + fibo(n-2);
+	}
+	
+	private static void spinWaiting() {
+		final AtomicInteger i = new AtomicInteger(0);
+		new Thread(()->{
+			i.set(fibo(30));
+		}).start();
+		
+		while(i.get() == 0);
+		System.out.println("spinWaiting:" + i);
+	}
+	
+	private static Object lock = new Object();
+	private static void useWaitAndNotify() throws InterruptedException {
+		final AtomicInteger i = new AtomicInteger(0);
+		new Thread(()->{
+			synchronized (lock) {				
+				i.set(fibo(30));
+				lock.notify();
+			}
+			
+		}).start();
+		synchronized (lock) {
+			lock.wait();
+		}
+		System.out.println("useWaitAndNotify:" + i);
+	}
+	
+	private static void useJoin() throws InterruptedException {
+		final AtomicInteger i = new AtomicInteger(0);
+		Thread t = new Thread(()->{
+			i.set(fibo(30));
+		});
+		t.start();
+		t.join();
+		System.out.println("useJoin:" + i);
+	}
+	
+	private static void useFuture() throws InterruptedException, ExecutionException {
+		Future<Integer> f = exec.submit(()->{
+			return fibo(29);
+		});
+		
+		System.out.println("useFuture:" + f.get());
+	}
+	
+	private static void useCompletableFuture() throws InterruptedException, ExecutionException {
+		CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(()->{
+			return fibo(30);
+		});
+		System.out.println("useCompletableFuture:" + cf.get());
+	}
+	
+	private static void useSemaphore() throws InterruptedException {
+		Semaphore s = new Semaphore(2);
+		final AtomicInteger i = new AtomicInteger(0);
+		s.acquire();
+		new Thread(()->{
+			i.set(fibo(30));
+			s.release();
+		}).start();
+		s.acquire(2);
+		System.out.println("useSemaphore:" + i);
+	}
+	
+	private static void useCountDownLatch() throws InterruptedException {
+		CountDownLatch cdl = new CountDownLatch(1);
+		final AtomicInteger i = new AtomicInteger(0);
+		new Thread(()->{
+			i.set(fibo(30));
+			cdl.countDown();
+		}).start();
+		cdl.await();
+		System.out.println("useCountDownLatch:" + i);
+	}
+	
+	private static void useCyclicBarrier() throws InterruptedException, BrokenBarrierException {
+		final AtomicInteger i = new AtomicInteger(0);
+		CyclicBarrier cb = new CyclicBarrier(2, ()->{
+			System.out.println("useCyclicBarrier:" + i);
+		});
+		new Thread(()->{
+			i.set(fibo(30));
+			try {
+				cb.await();
+			} catch (InterruptedException | BrokenBarrierException e) {
+				e.printStackTrace();
+			}
+		}).start();
+		cb.await();
+	}
+}
