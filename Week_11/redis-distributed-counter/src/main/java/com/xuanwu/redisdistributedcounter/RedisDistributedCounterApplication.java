bbd1bd00605033b0cdf7f59bbd0350bddf7e63f2
huangwl <huangwl@kamfu.com.cn>
2020-12-29 22:15:57 +0800

    增加week11作业

---
+++ Week_11/redis-distributed-counter/src/main/java/com/xuanwu/redisdistributedcounter/RedisDistributedCounterApplication.java
@@ -0,0 +1,13 @@
+package com.xuanwu.redisdistributedcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDistributedCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDistributedCounterApplication.class, args);
+	}
+
+}
